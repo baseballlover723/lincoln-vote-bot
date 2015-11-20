5130536ff97bac9915191f07457f0ca378db5573
rosspa <rosspa@rose-hulman.edu>
2015-11-20 04:28:18 -0800

    put the real form link in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,13 @@
 require "concurrent"
 class MainController < ApplicationController
   @@votes_left = 0
+  # @@form_url = "https://docs.google.com/forms/d/1XnhCLV_BKFUD9vOcJdtskRzjpCHap__sp_vhyqwJvjo/viewform"
+  @@form_url = "https://docs.google.com/forms/d/19FfLH0u7x4L13Uh-BoEFmnCybM4WMWG6jprDNDtrVZE/viewform"
   def index
     @total_votes = Vote.first.count
     @max_votes = 200
     @votes_left = @@votes_left
+    @form_url = @@form_url
   end
 
   def submit_votes
@@ -19,8 +22,7 @@ class MainController < ApplicationController
   def vote(number_of_times, randomize)
     agent = Mechanize.new
     agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    agent.get "https://docs.google.com/forms/d/1XnhCLV_BKFUD9vOcJdtskRzjpCHap__sp_vhyqwJvjo/viewform"
-    # agent.get("https://docs.google.com/forms/d/19FfLH0u7x4L13Uh-BoEFmnCybM4WMWG6jprDNDtrVZE/viewform")
+    agent.get @@form_url
     form = agent.page.forms[0]
     form.radiobutton_with(:value => /Lincoln High School/).check
     number_of_times.times do
