61b612aeb7e56892d7803eaea5856db8b53d98c3
rosspa <rosspa@rose-hulman.edu>
2015-11-20 04:16:47 -0800

    initial commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,39 @@
+require "concurrent"
+class MainController < ApplicationController
+  @@votes_left = 0
+  def index
+    @total_votes = Vote.first.count
+    @max_votes = 200
+    @votes_left = @@votes_left
+  end
+
+  def submit_votes
+    number_of_votes = params["vote"]["number"].to_i
+    randomize = params["randomize"] ? true : false
+    @@votes_left += number_of_votes
+    Concurrent::Future.execute { vote(number_of_votes, randomize) }
+    index
+    render action: "index"
+  end
+
+  def vote(number_of_times, randomize)
+    agent = Mechanize.new
+    agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    agent.get "https://docs.google.com/forms/d/1XnhCLV_BKFUD9vOcJdtskRzjpCHap__sp_vhyqwJvjo/viewform"
+    # agent.get("https://docs.google.com/forms/d/19FfLH0u7x4L13Uh-BoEFmnCybM4WMWG6jprDNDtrVZE/viewform")
+    form = agent.page.forms[0]
+    form.radiobutton_with(:value => /Lincoln High School/).check
+    number_of_times.times do
+      Vote.first.increment!(:count, 1)
+      form.submit
+      @@votes_left -= 1
+      if randomize
+        rand = 5 * rand()
+        puts "sleeping for #{rand}"
+        sleep rand
+      end
+    end
+
+
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_lincoln_vote_bot_session'
